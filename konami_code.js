f58b010aab49d05f437d1b7befb44453cf40b166
Kah Yap <kahyap13@gmail.com>
2018-11-16 04:06:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,24 @@ const codes = [
 ];
 
 function init() {
-  // your code here
-}
+    // your code here
+    let counter = 0
+    let yes = 0
+    
+    body.addEventListener('keydown', x => {
+      let key = x.key;
+  
+      if(key === codes[counter]) {
+          yes++;
+          counter++;
+      } else {
+          yes = 0
+          counter = 0
+      }
+      
+      if (yes === codes.length) alert('congradulations')
+        
+      })
+  }
+
+init()
